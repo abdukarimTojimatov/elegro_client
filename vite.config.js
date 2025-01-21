c947257e3fa85e81285d69842dbe3a855ffd12ee
Tojimatov Abdukarim <walterbriendeveloper@gmail.com>
2025-01-21 14:03:08 +0500

    feat: changed portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default {
   server: {
-    port: 3000,
+    host: "0.0.0.0", // Listen on all interfaces
+    port: 3000, // Ensure it's set to port 3000
   },
-});
+};
